69ef6cded501987d5851f1714c9db0f951a79745
Mike Timashov <mike@cherrypiestudio.com>
2025-10-11 15:44:44 +0200

    Vercel build fix

--- light-2.0-web/next.config.ts
+++ light-2.0-web/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Exclude supabase functions from webpack compilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/supabase/**'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
